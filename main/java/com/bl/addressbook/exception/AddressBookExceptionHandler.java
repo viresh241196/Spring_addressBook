8ca79d63fee3b408ad76318a909f916c3a314991
viresh241196 <vireshrawool007@gmail.com>
2021-05-02 18:43:32 +0530

    create a exception class and method

---
+++ main/java/com/bl/addressbook/exception/AddressBookExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.bl.addressbook.exception;
+
+
+import com.bl.addressbook.DTO.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class AddressBookExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList =exception.getBindingResult().getAllErrors();
+        List<String> errorMessage= errorList.stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO responseDTO=
+                new ResponseDTO("Exception while processing the request",errorMessage);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
